aefcc34865da920f6996d2cde154b8e6c2a602b2
David_castrillon <cryptofinanzas@icloud.com>
2022-09-09 10:58:49 +0200

    ¨add

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
-console.log("David")
+console.log("David Castrillon")
 
+setTimeout (wakeUpUser, 5000) ;
+function wakeUpUser () {
+alert ("Are you going to stare at this boring page forever?")
 
-food = 20
-console.log(food); 
+}
 
+var price = 28.99;
+var discount = 10;
+var total =
+    price - (price * (discount / 100));
+    if (total > 25) {
+        freeShipping()
+    }
